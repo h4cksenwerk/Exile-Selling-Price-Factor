bbcbbce3c77165c3dccb09ceb9a8dc821df3c1f9
Daniel M. Gehrlein <dmg.lab@outlook.com>
2017-05-05 22:15:16 +0200

    add CfgExileCustomCode overwrites

---
+++ mpmissions/Exile.Altis/config.cpp
@@ -0,0 +1,13 @@
+class CfgExileCustomCode 
+{
+    // H4cksenwerk Selling Price Factor
+    ExileClient_gui_traderDialog_event_onStoreListBoxSelectionChanged = "h4xwk_spf/ExileClient_gui_traderDialog_event_onStoreListBoxSelectionChanged.sqf";
+    ExileClient_gui_traderDialog_updateStoreListBox = "h4xwk_spf/ExileClient_gui_traderDialog_updateStoreListBox.sqf";
+    ExileClient_gui_vehicleTraderDialog_event_onInputBoxChars = "h4xwk_spf/ExileClient_gui_vehicleTraderDialog_event_onInputBoxChars.sqf";
+    ExileClient_gui_vehicleTraderDialog_updateVehicle = "h4xwk_spf/ExileClient_gui_vehicleTraderDialog_updateVehicle.sqf";
+    ExileClient_gui_vehicleTraderDialog_updateVehicleListBox = "h4xwk_spf/ExileClient_gui_vehicleTraderDialog_updateVehicleListBox.sqf";
+    ExileClient_util_gear_calculateSellPrice = "h4xwk_spf/ExileClient_util_gear_calculateSellPrice.sqf";
+    ExileClient_util_gear_calculateTotalPrice = "h4xwk_spf/ExileClient_util_gear_calculateTotalPrice.sqf";
+    ExileServer_system_trading_network_purchaseVehicleRequest = "h4xwk_spf/ExileServer_system_trading_network_purchaseVehicleRequest.sqf";
+    ExileServer_system_trading_network_purchaseItemRequest = "h4xwk_spf/ExileServer_system_trading_network_purchaseItemRequest.sqf";
+}
